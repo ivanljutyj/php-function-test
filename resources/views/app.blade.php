7bcedbabcd7f1f3899a954ac3c1d2ab159b72dc8
Ivan Ivanoff <contact@ivanon.io>
2020-02-29 12:03:11 -0500

    Add bootstrap basic theme

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
+<head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -8,6 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <!-- Styles -->
         <style>
@@ -64,11 +66,27 @@
         </style>
     </head>
     <body>
-        <div id="app">
-            <example-component></example-component>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">PHP Function Test</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
         </div>
-    </body>
-    <link href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}"></script>
+    </nav>
 
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 mt-5">
+                <div id="app">
+                    <example-component></example-component>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+    </body>
 </html>
